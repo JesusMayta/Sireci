534c84b86f3057cc5ca4a4d99331c84edd2bb8af
Emir Huaroc <emirmho3@gmail.com>
2024-02-08 17:11:53 -0500

    maquetando registro de matrimonio

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../login';
 import { RegisterPage } from '../register';
 import { ProfilePage } from '../profile';
+import { FormMatrimonio } from '../matrimonio/views/FormMatrimonio';
 
 export const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
             <Route path="/" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path='/profile' element={<ProfilePage/>}/>
+            <Route path='/matrimonio' element={<FormMatrimonio/>}/>
         </Routes>
     )
 }
