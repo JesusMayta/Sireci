45443008f5a32ae9fb8c9076263afbd8b759a8c3
Emir Huaroc <emirmho3@gmail.com>
2024-02-22 12:24:47 -0500

    Registrar personas

--- src/helpers/links.ts
+++ src/helpers/links.ts
@@ -1,17 +1,19 @@
-import { FcServices, FcPlus, FcConferenceCall, FcLike } from 'react-icons/fc';
+import { FcServices, FcPlus, FcConferenceCall, FcLike,  FcReading } from 'react-icons/fc';
 import { GiDeathSkull } from "react-icons/gi";
 
 export const routes = [
     { id: 1, to: '/nacimiento', name: 'Nacimiento', icon: FcPlus },
     { id: 2, to: '/matrimonio', name: 'Matrimonio', icon: FcLike },
     { id: 3, to: '/', name: 'Defunción', icon: GiDeathSkull },
-    { id: 4, to: '/mantenimiento', name: 'Mantenimiento', icon: FcServices }
+    { id: 4, to: '/mantenimiento', name: 'Mantenimiento', icon: FcServices },
+    { id: 5, to: '/personas', name: 'Personas', icon: FcReading }
+
 ];
 
 export const SideLinks = {
     'user': [...routes],
     'admin': [
         ...routes,
-        { id: 5, to: '/usuarios', name: 'Usuarios', icon: FcConferenceCall },
+        { id: 6, to: '/usuarios', name: 'Usuarios', icon: FcConferenceCall },
     ]
 };
