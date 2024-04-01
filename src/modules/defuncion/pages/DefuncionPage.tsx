d5d9bd696a5a989dfb6428bbfff00495ce0bd819
Conrad <conradbravina@gmail.com>
2024-04-01 07:49:44 -0700

    Avance de los estilos

--- src/modules/defuncion/pages/DefuncionPage.tsx
+++ src/modules/defuncion/pages/DefuncionPage.tsx
@@ -1,6 +1,13 @@
-import { FormLayout, PrincipalLayout, PrincipalView } from '../../layouts';
+import {
+  FormLayout,
+  PrincipalLayout,
+  PrincipalView,
+  PrincipalViewContainer,
+} from '../../layouts';
 import { useUiStore } from '../../../hooks';
 import { TableDeathDocs, FormRegister, ModalUpdateDeath } from '../components';
+
+const SortBy = ['Dni', 'Nombres', 'Libro'];
 
 export const DefuncionPage = () => {
   const { isOpenViewForm, isOpenEditModal } = useUiStore();
@@ -13,9 +20,16 @@ export const DefuncionPage = () => {
     >
       <>
         {isOpenViewForm ? (
-          <FormLayout infoText='Es el procedimiento donde se registra el fallecimiento de una persona, para ello debe contar con el Certificado de Defunción, el cual tiene que estar debidamente firmado y sellado por profesional de salud o Declaración Jurada de la autoridad política, judicial o religiosa, en los lugares donde no existe un profesional de salud que acredite la defunción.'>
-            <FormRegister />
-          </FormLayout>
+          <PrincipalView
+            SortBy={SortBy}
+            placeHolder='Buscar al fallecido'
+            textButton='Nueva acta'
+            page='defuncion'
+          >
+            <FormLayout infoText='Es el procedimiento donde se registra el fallecimiento de una persona, para ello debe contar con el Certificado de Defunción, el cual tiene que estar debidamente firmado y sellado por profesional de salud o Declaración Jurada de la autoridad política, judicial o religiosa, en los lugares donde no existe un profesional de salud que acredite la defunción.'>
+              <FormRegister />
+            </FormLayout>
+          </PrincipalView>
         ) : (
           <PrincipalView
             SortBy={['DNI', 'Nombres', 'Libro']}
--- src/modules/users/pages/UsersPage.tsx
+++ src/modules/users/pages/UsersPage.tsx
@@ -1,26 +1,36 @@
-
 import { useUiStore } from '../../../hooks';
-import { FormLayout, PrincipalLayout, PrincipalView } from '../../layouts';
+import {
+  FormLayout,
+  PrincipalLayout,
+  PrincipalView,
+  PrincipalViewContainer,
+} from '../../layouts';
 import { TableUsers, FormRegisterUser } from '../components';
 
 export const UsersPage = () => {
+  const { isOpenViewForm } = useUiStore();
 
-    const { isOpenViewForm } = useUiStore();
-
-    return (
-        <PrincipalLayout title={(isOpenViewForm) ? 'Registrar nuevo usuario' : 'Lista de Usuarios'}>
-            <>
-                {(isOpenViewForm) ?
-                    (
-                        <FormLayout infoText="En esta sección es importante que usted digite bien los datos de un usuario nuevo, para que asi pueda realizar sus actividades en la aplicación.">
-                            <FormRegisterUser />
-                        </FormLayout>
-                    ) : (
-                        <PrincipalView SortBy={['Nombres', 'Apellidos', 'Username', 'Rol']} placeHolder='Buscar por dni, nombres, email y rol' textButton='Usuario'>
-                            <TableUsers />
-                        </PrincipalView>
-                    )}
-            </>
-        </PrincipalLayout>
-    );
+  return (
+    <PrincipalLayout
+      title={isOpenViewForm ? 'Registrar nuevo usuario' : 'Lista de Usuarios'}
+    >
+      <>
+        {isOpenViewForm ? (
+          <PrincipalViewContainer>
+            <FormLayout infoText='En esta sección es importante que usted digite bien los datos de un usuario nuevo, para que asi pueda realizar sus actividades en la aplicación.'>
+              <FormRegisterUser />
+            </FormLayout>
+          </PrincipalViewContainer>
+        ) : (
+          <PrincipalView
+            SortBy={['Nombres', 'Apellidos', 'Username', 'Rol']}
+            placeHolder='Buscar por dni, nombres, email y rol'
+            textButton='Usuario'
+          >
+            <TableUsers />
+          </PrincipalView>
+        )}
+      </>
+    </PrincipalLayout>
+  );
 };
