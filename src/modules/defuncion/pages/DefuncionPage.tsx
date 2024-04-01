266450142b06b7667db12de6969a70d9021bb7c6
Conrad <conradbravina@gmail.com>
2024-04-01 08:13:02 -0700

    Finalice los homogenizacion de estilos

--- src/modules/defuncion/pages/DefuncionPage.tsx
+++ src/modules/defuncion/pages/DefuncionPage.tsx
@@ -20,22 +20,16 @@ export const DefuncionPage = () => {
     >
       <>
         {isOpenViewForm ? (
-          <PrincipalView
-            SortBy={SortBy}
-            placeHolder='Buscar al fallecido'
-            textButton='Nueva acta'
-            page='defuncion'
-          >
+          <PrincipalViewContainer>
             <FormLayout infoText='Es el procedimiento donde se registra el fallecimiento de una persona, para ello debe contar con el Certificado de Defunción, el cual tiene que estar debidamente firmado y sellado por profesional de salud o Declaración Jurada de la autoridad política, judicial o religiosa, en los lugares donde no existe un profesional de salud que acredite la defunción.'>
               <FormRegister />
             </FormLayout>
-          </PrincipalView>
+          </PrincipalViewContainer>
         ) : (
           <PrincipalView
-            SortBy={['DNI', 'Nombres', 'Libro']}
+            SortBy={SortBy}
             placeHolder='Buscar por dni, nombres y libro'
             textButton='Nueva acta'
-            page='death'
           >
             <TableDeathDocs />
           </PrincipalView>
